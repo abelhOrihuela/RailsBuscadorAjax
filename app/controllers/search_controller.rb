1ee58e88a3bc0583b61239ca0b12039419106c12
Abel Orihuela <abelorihuelamendoza@hotmail.com>
2016-01-16 23:35:21 -0600

    Configuration controller->search- view->index

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,12 @@
 class SearchController < ApplicationController
   def create
-    
+    palabra = "%#{params[:keyword]}%"
+
+    @products= Product.all.where("name LIKE ? OR description LIKE ?", palabra, palabra)
+
+    respond_to do |format|
+      format.json { render json: @products }
+      format.html { redirect_to root_path }
+    end
   end
 end
