c894051ba77255ce6e5cce15ff4e64fc65fb1266
Abel Orihuela <abelorihuelamendoza@hotmail.com>
2016-01-21 23:16:24 -0600

    Response ajax Create ->partial create.js.coffe & _product.html.erb

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,10 +3,12 @@ class SearchController < ApplicationController
     palabra = "%#{params[:keyword]}%"
 
     @products= Product.all.where("name LIKE ? OR description LIKE ?", palabra, palabra)
-
+    puts @products.first.name + "*" *20
     respond_to do |format|
       format.json { render json: @products }
       format.html { redirect_to root_path }
+      format.js
+      #rails buscara un archivo create.js en relacion con el controlador
     end
   end
 end
